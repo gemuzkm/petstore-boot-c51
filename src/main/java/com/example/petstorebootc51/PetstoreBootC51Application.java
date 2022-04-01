e281ab996a628f378188b42159a35c303024dbfa
gemuzkm <gemuzkm@gmail.com>
2022-04-01 20:58:00 +0300

    Initial commit

---
+++ src/main/java/com/example/petstorebootc51/PetstoreBootC51Application.java
@@ -0,0 +1,13 @@
+package com.example.petstorebootc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreBootC51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetstoreBootC51Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petstorebootc51/PetstoreBootC51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petstorebootc51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreBootC51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
