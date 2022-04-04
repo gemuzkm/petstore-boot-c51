d54d1d1fa35cfcdeb2da0f4ca19f885d381740bf
gemuz_work <gemuzkm@gmail.com>
2022-04-04 09:40:34 +0300

    add interceptor

---
+++ src/main/java/com/example/petstorebootc51/configuration/WebConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.petstorebootc51.configuration;
+
+import com.example.petstorebootc51.interceptor.ApiInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    private final ApiInterceptor apiInterceptor;
+
+    public WebConfiguration(ApiInterceptor apiInterceptor) {
+        this.apiInterceptor = apiInterceptor;
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user")
+                .excludePathPatterns("/swagger-ui/**")
+                .excludePathPatterns("/v2/**")
+                .excludePathPatterns("/swagger-resources/**")
+                .excludePathPatterns("/webjars/**");
+    }
+}
---
+++ src/main/java/com/example/petstorebootc51/interceptor/ApiInterceptor.java
@@ -0,0 +1,28 @@
+package com.example.petstorebootc51.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class ApiInterceptor implements HandlerInterceptor {
+    public static final String HEADER_X_API_TOKEN = "X-API-Token";
+
+//    private final UserRepository userRepository;
+//
+//    public ApiInterceptor(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        if (userRepository.findByToken(request.getHeader(HEADER_X_API_TOKEN)).isPresent()) {
+//            return true;
+//        }
+        return true;
+//        response.setStatus(401);
+//        return false;
+    }
+}
