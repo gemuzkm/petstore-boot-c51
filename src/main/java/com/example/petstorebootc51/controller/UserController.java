b1d7add7b93a2ecbc2dd4882b78a916f7adcd3e8
gemuz_work <gemuzkm@gmail.com>
2022-04-04 15:37:11 +0300

    UserController fix

--- src/main/java/com/example/petstorebootc51/controller/UserController.java
+++ src/main/java/com/example/petstorebootc51/controller/UserController.java
@@ -2,35 +2,34 @@ package com.example.petstorebootc51.controller;
 
 import com.example.petstorebootc51.entity.User;
 import com.example.petstorebootc51.repository.UserRepository;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @ApiOperation(value="Create user")
-    @PostMapping
-    public ResponseEntity<User> save(@RequestBody User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        User save = userRepository.save(user);
-        return ResponseEntity.ok(save);
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    @ApiOperation(value="Get user by user name")
+    @ApiOperation(value = "Create user", notes = "This can only be done by the logged in user.")
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody User user, BindingResult bindingResult) {
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @ApiOperation(value = "Get user by user name")
     @GetMapping("/{username}")
     public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) {
         if (username == null | userRepository.getUserByUsername(username).isEmpty()) {
@@ -38,7 +37,7 @@ public class UserController {
         }
 
         Optional<User> userByUsername = userRepository.getUserByUsername(username);
-        return  ResponseEntity.ok(userByUsername.get());
+        return ResponseEntity.ok(userByUsername.get());
     }
 
 //    @PutMapping("/{username}")
@@ -53,7 +52,7 @@ public class UserController {
 //
 //    }
 
-    @ApiOperation(value="Delete user")
+    @ApiOperation(value = "Delete user")
     @DeleteMapping("/{username}")
     public void deleteUserByUsername(@PathVariable("username") String username) {
         if (username == null | userRepository.getUserByUsername(username).isEmpty()) {
@@ -64,7 +63,7 @@ public class UserController {
         userByUsername.ifPresent(user -> userRepository.delete(user));
     }
 
-    @ApiOperation(value="Logs user into the system")
+    @ApiOperation(value = "Logs user into the system")
     @GetMapping("/login")
     public String login(String username, String password, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
